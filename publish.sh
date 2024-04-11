a477a472f9502d8ff3beab94c2d0db69c53218f5
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 22:13:23 +0800

    hhhh

--- publish.sh
+++ publish.sh
@@ -7,11 +7,13 @@ GPG_PASSPHRASE = "$2"
 MAVEN_USER_NAME = "$3"
 MAVEN_PASSWORD = "$4"
 
-echo $GPG_PRIVATE_KEY >> private.key
+echo "$GPG_PRIVATE_KEY"
+
+echo "$GPG_PRIVATE_KEY" >> private.key
 
 gpg --batch --import private.key
 
 gpg --list-keys
 
 # 执行 Maven 构建并发布到 Maven 中央仓库
-mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=$GPG_PASSPHRASE --settings .github/maven-settings.xml --file pom.xml
+# mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=$GPG_PASSPHRASE --settings .github/maven-settings.xml --file pom.xml
