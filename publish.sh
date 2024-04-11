40f90ba97d995a5f087058f8b5aab5d35692bf80
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 16:26:23 +0800

    wqeqweqw

--- publish.sh
+++ publish.sh
@@ -2,25 +2,15 @@
 
 # 导入已存在的 GPG 密钥
 gpg --batch --import public.key
-
 gpg --batch --import private.key
-
 gpg --list-keys
 
-export GPG_TTY=zhoujunlin
 
 # 执行 Maven 构建并发布到 Maven 中央仓库
-
 # mvn --batch-mode clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml --file pom.xml -X
 
-cd /github/workspace/
-
-mvn help:evaluate -Dexpression=project.build.directory -q -DforceStdout
-
 mvn clean package -X
-
 mvn help:evaluate -Dexpression=project.build.directory -q -DforceStdout
-
 
 ls -l /github/workspace/target/
 ls -l /github/workspace/target/central-publishing
