716f14f9a8661da3f30f16a02fcb23a76f697eca
zhoujunlin <zhoujunlin@gaodun.com>
2024-04-11 16:19:18 +0800

    1111

---
+++ src/main/java/io/github/zhoujunlin94/autodeploy/AutoDeployApplication.java
@@ -0,0 +1,18 @@
+package io.github.zhoujunlin94.autodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoujunlin
+ * @date 2024年04月11日 16:16
+ * @desc
+ */
+@SpringBootApplication
+public class AutoDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoDeployApplication.class, args);
+    }
+
+}
