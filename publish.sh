f134a4146fdc0b737c2418317c49f88db909dc3e
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 15:38:37 +0800

    yyyy

--- publish.sh
+++ publish.sh
@@ -10,7 +10,7 @@ gpg --list-keys
 export GPG_TTY=zhoujunlin
 
 # 执行 Maven 构建并发布到 Maven 中央仓库
-mvn --batch-mode clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml
+mvn --batch-mode clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml --file pom.xml
 
 ls -l /github/workspace/target/central-publishing
 ls -l /github/workspace/target/central-staging/io/github/zhoujunlin94/auto_deploy/1.0.0
