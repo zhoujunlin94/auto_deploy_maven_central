88fb7de8419b68414d85bb34316203c25ae3cc9a
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 14:04:09 +0800

    test

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM maven:3.6.3-openjdk-11
+
+# 将发布脚本添加到容器中
+COPY publish.sh /publish.sh
+
+# 设置可执行权限
+RUN chmod +x /publish.sh
+
+# 运行发布脚本
+ENTRYPOINT ["/publish.sh"]
---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 导入已存在的 GPG 密钥
+gpg --import public.key
+gpg --import private.key
+
+
+# 执行 Maven 构建并发布到 Maven 中央仓库
+mvn clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml
