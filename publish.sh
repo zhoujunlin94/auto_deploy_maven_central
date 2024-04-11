5e812e566bc2058f38a2aad633868bd0f48593fb
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 16:59:55 +0800

    qqqqqqqqqqqqq

--- publish.sh
+++ publish.sh
@@ -11,8 +11,7 @@ export GPG_TTY=zhoujunlin
 chmod -R +x /github/workspace
 
 # 执行 Maven 构建并发布到 Maven 中央仓库
-mvn --batch-mode clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml --file pom.xml -X
-
+mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml --file pom.xml -X
 
 
 # mvn clean install -X
