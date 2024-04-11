a9099081999814465064b10fc05742d6b29e8f52
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 22:28:47 +0800

    github

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
-FROM maven:3.8.1-openjdk-8
+# FROM maven:3.8.1-openjdk-8
 
-# 将发布脚本添加到容器中
+# # 将发布脚本添加到容器中
+# COPY publish.sh /publish.sh
+
+# # 设置可执行权限
+# RUN chmod +x /publish.sh
+
+# # 运行发布脚本
+# ENTRYPOINT ["/publish.sh"]
+
+
+# Container image that runs your code
+FROM alpine:3.10
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
 COPY publish.sh /publish.sh
 
-# 设置可执行权限
-RUN chmod +x /publish.sh
-
-# 运行发布脚本
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/publish.sh"]
--- publish.sh
+++ publish.sh
@@ -1,21 +1,30 @@
-#!/bin/bash
+# #!/bin/bash
 
-# 导入已存在的GPG私钥
+# # 导入已存在的GPG私钥
 
-GPG_PRIVATE_KEY=$1
-GPG_PASSPHRASE=$2
-MAVEN_USER_NAME=$3
-MAVEN_PASSWORD=$4
+# GPG_PRIVATE_KEY=$1
+# GPG_PASSPHRASE=$2
+# MAVEN_USER_NAME=$3
+# MAVEN_PASSWORD=$4
 
-echo "$GPG_PRIVATE_KEY"
+# echo "$GPG_PRIVATE_KEY"
 
-echo "$GPG_PRIVATE_KEY" >> private.key
+# echo "$GPG_PRIVATE_KEY" >> private.key
 
-cat private.key
+# cat private.key
 
-gpg --batch --import private.key
+# gpg --batch --import private.key
 
-gpg --list-keys
+# gpg --list-keys
 
-# 执行 Maven 构建并发布到 Maven 中央仓库
-# mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=$GPG_PASSPHRASE --settings .github/maven-settings.xml --file pom.xml
+# # 执行 Maven 构建并发布到 Maven 中央仓库
+# # mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=$GPG_PASSPHRASE --settings .github/maven-settings.xml --file pom.xml
+
+
+
+#!/bin/sh -l
+
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
+
