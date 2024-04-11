97575e41c208d7c36761da8d33a082c2036433ad
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-11 14:13:21 +0800

    test

--- publish.sh
+++ publish.sh
@@ -6,4 +6,4 @@ gpg --import private.key
 
 
 # 执行 Maven 构建并发布到 Maven 中央仓库
-mvn clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml
+mvn clean deploy -DskipTests=true -Dgpg.passphrase=zhoujunlin --settings .github/maven-settings.xml  -X
