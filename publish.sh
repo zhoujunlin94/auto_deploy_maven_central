7c2ce184e3524da2f5bbcfe8c9b199820c4e8fb5
zhoujunlin <134687516+zhoujunlin94@users.noreply.github.com>
2024-04-12 15:18:06 +0800

    ggggggggggggg

--- publish.sh
+++ publish.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # 导入已存在的GPG私钥
-
-cat private.key
-
-# echo $INPUT_GPG_PRIVATE_KEY >> private.key
-
 gpg --batch --import private.key
 
 gpg --list-keys
 
 # 执行 Maven 构建并发布到 Maven 中央仓库
-# mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=$GPG_PASSPHRASE --settings .github/maven-settings.xml --file pom.xml
+mvn --batch-mode clean package verify deploy -DskipTests=true -Dgpg.passphrase=$INPUT_GPG_PASSPHRASE --settings .github/maven-settings.xml --file pom.xml
